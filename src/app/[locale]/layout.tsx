0ea9a882307dddeee9a446def7b20272733ef1a3
im-beltagy <imbeltagy@gmail.com>
2025-05-26 23:26:01 +0300

    chor(seo): add default metadata

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -35,5 +35,12 @@ export async function generateMetadata({ params }: { params: Promise<{ locale: L
   return {
     title: t('title'),
     description: t('description'),
+    manifest: '/manifest.json',
+    icons: [
+      { rel: 'icon', url: '/favicon/favicon.ico' },
+      { rel: 'icon', type: 'image/png', sizes: '16x6', url: '/favicon/favicon-16x16.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x12', url: '/favicon/favicon-32x32.png' },
+      { rel: 'apple-touch-icon', sizes: '180x67', url: '/favicon/apple-touch-icon.png' },
+    ],
   };
 }
